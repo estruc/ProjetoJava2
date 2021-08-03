1b68ec65894d1ea09e82dcb9d328a082efb90d89
marce <marce@DESKTOP-4H6STEG>
2021-08-03 19:30:17 -0300

    primeiro deploy

---
+++ PrimeiroProjeto2/src/main/java/br/com/senac/PrimeiroProjeto2Application.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjeto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjeto2Application.class, args);
+	}
+
+}
